4d6f44aab43340dad396b0e01382fbc80f07f3c6
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-10-03 14:14:48 +0100

    Wetting-and-drying option included but not yet functional

--- FixedMesh.py
+++ FixedMesh.py
@@ -9,13 +9,13 @@ import utils.domain as dom
 
 print('\n******************************** FIXED MESH TSUNAMI SIMULATION ********************************\nOptions...')
 
-# Set up mesh and initialise variables:
+# Set up mesh, initialise variables and specify parameters:
 mesh, eta0, b = dom.TohokuDomain(res=int(input('Mesh coarseness (integer in range 1-5, default 4)?: ') or 4))
-
-# Specify time parameters:
 dt = float(input('Specify timestep (s) (default 1):') or 1.)
-ndump = 60      # Inverse data dump frequency
-T = 3600        # Simulation time period (s) of 1 hour
+ndump = 60                                                                  # Inverse data dump frequency
+T = float(input('Specify time period (mins) (default 60):') or 60.) * 60    # Default 1 hour simulation time period (s)
+wd = bool(input('Press anything except enter to consider wetting-and-drying. '))
+print('\n')                                                                 # End of options.
 
 # Construct solver:
 solver_obj = solver2d.FlowSolver2d(mesh, b)
@@ -25,6 +25,9 @@ options.simulation_end_time = T
 options.timestepper_type = 'CrankNicolson'
 options.timestep = dt
 options.output_directory = 'plots/fixedMesh'
+if wd:
+    print('Using wetting-and-drying')
+    options.use_wetting_and_drying = True
 
 # Apply ICs:
 solver_obj.assign_initial_conditions(elev=eta0)
