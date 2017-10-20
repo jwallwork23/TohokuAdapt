2b348d1a20a97c2735653330879600f637e6fc78
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-10-20 11:46:51 +0100

    Notation changes for clarity

--- SimpleAdapt.py
+++ SimpleAdapt.py
@@ -15,14 +15,13 @@ print('Mesh adaptive solver initially defined on a mesh of',)
 
 # Define initial mesh:
 mesh, eta0, b = dom.TohokuDomain(int(input('coarseness (Integer in range 1-5, default 5): ') or 5))
-N1 = len(mesh.coordinates.dat.data)     # Minimum number of vertices
-N2 = N1                                 # Maximum number of vertices
-SumN = N1                               # Sum over vertex counts
-print('...... mesh loaded. Initial number of vertices : ', N1)
+N = [len(mesh.coordinates.dat.data), len(mesh.coordinates.dat.data)]    # Min/max number of vertices
+SumN = N[0]                                                             # Sum over vertex counts
+print('...... mesh loaded. Initial number of vertices : ', N[0])
 
 # Get default adaptivity parameter values:
 op = opt.Options()
-numVer = op.vscale * N1
+numVer = op.vscale * N[0]
 hmin = op.hmin
 hmax = op.hmax
 hmin2 = pow(hmin, 2)      # Square minimal side-length
@@ -47,7 +46,7 @@ if dt > cdt:
 ndump = op.ndump
 rm = op.rm
 
-# Initialise counters, files and gauge data measurements:
+# Initialise counters and filename:
 t = 0.
 dumpn = 0
 mn = 0
@@ -55,29 +54,32 @@ filename = 'plots/simpleAdapt/'
 
 tic1 = clock()
 while t < T - 0.5 * dt:
+    index = int(mn * rm / ndump)
     mn += 1
     tic2 = clock()
 
-    # Load variables from disk      TODO: make this process more general. 2 = rm / ndump
+    # Define discontinuous spaces on the new mesh:
     W1 = FunctionSpace(mesh, 'DG', 1)
     W2 = VectorFunctionSpace(mesh, 'DG', 1)
     if mn == 1:
+        # Enforce initial conditions on discontinuous space:
         elev_2d = Function(W1)
         elev_2d.interpolate(eta0)
         uv_2d = Function(W2)
         uv_2d.interpolate(Expression((0, 0)))
+    # Load variables from disk:
     elif mn < 5:
-        with DumbCheckpoint(filename+'hdf5/Elevation2d_0000{}'.format(mn * 2), mode=FILE_READ) as el:
+        with DumbCheckpoint(filename+'hdf5/Elevation2d_0000{}'.format(index), mode=FILE_READ) as el:
             elev_2d = Function(W1)
             el.load(elev_2d)
-        with DumbCheckpoint(filename+'hdf5/Velocity2d_0000{}'.format(mn * 2), mode=FILE_READ) as uv:
+        with DumbCheckpoint(filename+'hdf5/Velocity2d_0000{}'.format(index), mode=FILE_READ) as uv:
             uv_2d = Function(W2)
             uv.load(uv_2d)
     else:
-        with DumbCheckpoint(filename + 'hdf5/Elevation2d_000{}'.format(mn * 2), mode=FILE_READ) as el:
+        with DumbCheckpoint(filename + 'hdf5/Elevation2d_000{}'.format(index), mode=FILE_READ) as el:
             elev_2d = Function(W1)
             el.load(elev_2d)
-        with DumbCheckpoint(filename + 'hdf5/Velocity2d_000{}'.format(mn * 2), mode=FILE_READ) as uv:
+        with DumbCheckpoint(filename + 'hdf5/Velocity2d_000{}'.format(index), mode=FILE_READ) as uv:
             uv_2d = Function(W2)
             uv.load(uv_2d)
 
@@ -85,42 +87,23 @@ while t < T - 0.5 * dt:
     V = TensorFunctionSpace(mesh, 'CG', 1)
     if iso:
         M = Function(V)
-        if mtype == 's':
-            raise NotImplementedError('Adaption w.r.t. speed not yet implemented.')
-        elif mtype == 'f':
-            for i in range(len(M.dat.data)):
-                ielev2 = 1. / max(hmin2, min(pow(elev_2d.dat.data[i], 2), hmax2))
-                M.dat.data[i][0, 0] = ieta2
-                M.dat.data[i][1, 1] = ieta2
-        else:
-            raise NotImplementedError('Cannot currently interpret isotropic adaption with respect to two fields.')
+        for i in range(len(M.dat.data)):
+            ielev2 = 1. / max(hmin2, min(pow(elev_2d.dat.data[i], 2), hmax2))
+            M.dat.data[i][0, 0] = ieta2
+            M.dat.data[i][1, 1] = ieta2
     else:
-        H = Function(V)
-        if mtype != 'f':
-            raise NotImplementedError('Adaption w.r.t. speed not yet implemented.')
-        if mtype != 's':
-            H = adap.constructHessian(mesh, V, elev_2d, method=hessMeth)
-            M2 = adap.computeSteadyMetric(mesh, V, H, elev_2d, h_min=hmin, h_max=hmax, num=numVer, normalise=ntype)
-        if mtype == 'b':
-            raise NotImplementedError('Adaption w.r.t. two fields not yet implemented.')
-        else:
-            M = Function(V)
-            M.assign(M2)
+        H = adap.constructHessian(mesh, V, elev_2d, method=hessMeth)
+        M = adap.computeSteadyMetric(mesh, V, H, elev_2d, h_min=hmin, h_max=hmax, num=numVer, normalise=ntype)
 
     # Adapt mesh with respect to computed metric field and interpolate functions onto new mesh:
     adaptor = AnisotropicAdaptation(mesh, M)
     mesh = adaptor.adapted_mesh
-
-    # Interpolate variables onto new mesh:
     elev_2d, uv_2d, b = inte.interp(mesh, elev_2d, uv_2d, b)
 
     # Mesh resolution analysis:
     n = len(mesh.coordinates.dat.data)
     SumN += n
-    if n < N1:
-        N1 = n
-    elif n > N2:
-        N2 = n
+    N = [min(n, N[0]), max(n, N[1])]
 
     # Get solver parameter values and construct solver:     TODO: different FE space options
     solver_obj = solver2d.FlowSolver2d(mesh, b)
@@ -135,21 +118,20 @@ while t < T - 0.5 * dt:
     options.export_diagnostics = True
     options.fields_to_export_hdf5 = ['elev_2d', 'uv_2d']
 
-    # Apply ICs:
+    # Apply initial conditions:
     solver_obj.assign_initial_conditions(elev=elev_2d)
 
-    # Run the model:
+    # Time integrate:
     solver_obj.iterate()
-
     toc2 = clock()
 
     # Print to screen:
     print('\n************ Adaption step %d **************' % mn)
     print('Time = %1.2f mins / %1.1f mins' % (t / 60., T / 60.))
-    print('Number of vertices after adaption step %d: ' % mn, n)
-    print('Min/max vertex counts: %d, %d' % (N1, N2))
-    print('Mean vertex count: %d' % (float(SumN) / mn))
-    print('Elapsed time for this step: %1.2fs' % (toc2 - tic2), '\n')
+    print('#Vertices after adaption step %d: %d' % (mn, n))
+    print('Min/max #Vertices:', N)
+    print('Mean #Vertices: %d' % (float(SumN) / mn))
+    print('Elapsed time for this step: %1.2fs \n' % (toc2 - tic2))
 print('\a')
 toc1 = clock()
 print('Elapsed time for adaptive solver: %1.1fs (%1.2f mins)' % (toc1 - tic1, (toc1 - tic1) / 60))
