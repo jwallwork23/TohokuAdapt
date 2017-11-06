af10807be459ed69b638b338be2d3122c7400749
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-11-06 12:14:35 +0000

    Included fixedMesh test solver

--- testSuite.py
+++ testSuite.py
@@ -19,6 +19,7 @@ mesh = SquareMesh(n, n, lx, lx)
 x, y = SpatialCoordinate(mesh)
 W = VectorFunctionSpace(mesh, "DG", 1) * FunctionSpace(mesh, "CG", 2)
 b = Function(W.sub(1), name="Bathymetry").assign(0.1)
+eta0 = Function(W.sub(1), name="Initial surface").interpolate(1e-3 * exp(-(pow(x - np.pi, 2) + pow(y - np.pi, 2))))
 nEle, nVer = adap.meshStats(mesh)
 N = [nEle, nEle]    # Min/max #Elements
 print('... with #Elements : %d. Initial #Vertices : %d. \n' % (nEle, nVer))
@@ -30,7 +31,25 @@ T = op.T
 dt = op.dt
 op.checkCFL(b)
 ndump = op.ndump
-rm = 5      # Timesteps per simpleAdapt remeshes
+
+# Run fixedMesh forward solver
+dirName = "plots/tests/fixedMesh"
+solver_obj = solver2d.FlowSolver2d(mesh, b)
+options = solver_obj.options
+options.element_family = op.family
+options.use_nonlinear_equations = False
+options.simulation_export_time = dt * ndump
+options.simulation_end_time = T
+options.timestepper_type = op.timestepper
+options.timestep = dt
+options.output_directory = dirName
+options.export_diagnostics = True
+options.fields_to_export_hdf5 = ['elev_2d', 'uv_2d']
+solver_obj.assign_initial_conditions(elev=eta0)
+solver_obj.iterate()
+
+# Update parameters for simpleAdapt solver
+rm = 5
 dirName = 'plots/tests/simpleAdapt/'
 
 # Initialise counters
@@ -39,10 +58,6 @@ dumpn = 0
 mn = 0
 Sn = 0
 tic1 = clock()
-
-# TODO: run fixedMesh solver forward in time to t = T, storing to hdf5
-# TODO: run fixed mesh adjoint solver backward in time to t = 0, again storing to hdf5
-# TODO: run adjointBased solver
 
 while mn < np.ceil(T / (dt * rm)):
     tic2 = clock()
@@ -54,7 +69,7 @@ while mn < np.ceil(T / (dt * rm)):
     index = mn * int(rm / ndump)
     indexStr = stor.indexString(index)
     if mn == 0:
-        elev_2d.interpolate(1e-3 * exp(-(pow(x - np.pi, 2) + pow(y - np.pi, 2))))
+        elev_2d.assign(eta0)
         uv_2d.interpolate(Expression((0, 0)))
     else:
         with DumbCheckpoint(dirName + 'hdf5/Elevation2d_' + indexStr, mode=FILE_READ) as el:
@@ -111,3 +126,7 @@ Percent complete  : %4.1f%%    Elapsed time : %4.2fs (This step : %4.2fs)
           (mn, (100 * mn * rm * dt) / T, clock() - tic1, clock() - tic2, nEle, Sn / mn, N[0], N[1]))
 toc1 = clock()
 print('Elapsed time for simple adaptive solver: %1.1fs (%1.2f mins)' % (toc1 - tic1, (toc1 - tic1) / 60))
+
+# TODO: Update parameters for adjointBased solver
+# TODO: run fixed mesh adjoint solver backward in time to t = 0, again storing to hdf5
+# TODO: run adjointBased forward solver
