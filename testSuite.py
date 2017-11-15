b9d6caccb196a065a651dd9af4ecc7a3f9cf674e
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-11-15 11:25:57 +0000

    Updating testSuite

--- testSuite.py
+++ testSuite.py
@@ -8,11 +8,13 @@ from time import clock
 import utils.adaptivity as adap
 import utils.error as err
 import utils.interpolation as inte
+import utils.mesh as msh
 import utils.options as opt
 import utils.storage as stor
 
 print('\n******************************** SHALLOW WATER TEST PROBLEM ********************************\n')
 print('Mesh adaptive solver initially defined on a square mesh')
+basic = bool(input('Hit anything but enter to use basic error estimators, as in DL16: '))
 
 # Define inital mesh and function space
 n = 64
@@ -32,7 +34,6 @@ nVerT = op.vscale * nVer    # Target #Vertices
 T = op.T
 g = op.g
 dt = op.dt
-Dt = Constant(dt)
 op.checkCFL(b)
 ndump = op.ndump
 
@@ -48,6 +49,7 @@ solver_obj = solver2d.FlowSolver2d(mesh, b)
 options = solver_obj.options
 options.element_family = op.family
 options.use_nonlinear_equations = False
+options.use_grad_depth_viscosity_term = False
 options.simulation_export_time = dt * ndump
 options.simulation_end_time = T
 options.timestepper_type = op.timestepper
@@ -64,49 +66,33 @@ print('************ "SIMPLE" MESH ADAPTIVE SHALLOW WATER TEST ****************\n
 rm = 5
 dirName = 'plots/tests/simpleAdapt/'
 t = 0.
-mn = 0
-Sn = 0
+mn = Sn = 0
 tic1 = clock()
 
 while mn < np.ceil(T / (dt * rm)):
     tic2 = clock()
-
-    # Enforce initial conditions on discontinuous space / load variables from disk
-    W = VectorFunctionSpace(mesh, op.space1, op.degree1) * FunctionSpace(mesh, op.space2, op.degree2)
-    uv_2d = Function(W.sub(0))
-    elev_2d = Function(W.sub(1))
     index = mn * int(rm / ndump)
-    indexStr = stor.indexString(index)
-    if mn == 0:
-        elev_2d.assign(eta0)
-        uv_2d.interpolate(Expression((0, 0)))
-    else:
-        with DumbCheckpoint(dirName + 'hdf5/Elevation2d_' + indexStr, mode=FILE_READ) as el:
-            el.load(elev_2d, name='elev_2d')
-            el.close()
-        with DumbCheckpoint(dirName + 'hdf5/Velocity2d_' + indexStr, mode=FILE_READ) as ve:
-            ve.load(uv_2d, name='uv_2d')
-            ve.close()
+    elev_2d, uv_2d = op.loadFromDisk(mesh, index, dirName, elev0=eta0)      # Enforce ICs / load variables from disk
 
     # Compute Hessian and metric, adapt mesh and interpolate variables
-    if mn != 0:
-        V = TensorFunctionSpace(mesh, 'CG', 1)
-        M = adap.computeSteadyMetric(mesh, V, adap.constructHessian(mesh, V, elev_2d, op=op),
-                                     elev_2d, nVerT=nVerT, op=op)
-        mesh = AnisotropicAdaptation(mesh, M).adapted_mesh
-        elev_2d, uv_2d, b = inte.interp(mesh, elev_2d, uv_2d, b)
+    V = TensorFunctionSpace(mesh, 'CG', 1)
+    M = adap.computeSteadyMetric(mesh, V, H, elev_2d, nVerT=nVerT, op=op)
+    mesh = AnisotropicAdaptation(mesh, M).adapted_mesh
+    elev_2d, uv_2d, b = inte.interp(mesh, elev_2d, uv_2d, b)
+    if op.outputHessian:
+        H.rename("Hessian")
+        hfile.write(H, time=float(mn))
 
-    # Get solver parameter values and construct solver
+    # Establish Thetis flow solver object
     solver_obj = solver2d.FlowSolver2d(mesh, b)
     options = solver_obj.options
     options.element_family = op.family
     options.use_nonlinear_equations = False
+    options.use_grad_depth_viscosity_term = False
     options.simulation_export_time = dt * ndump
     options.simulation_end_time = (mn + 1) * dt * rm
     options.timestepper_type = op.timestepper
     options.timestep = dt
-
-    # Specify outfile directory and HDF5 checkpointing
     options.output_directory = dirName
     options.export_diagnostics = True
     options.fields_to_export_hdf5 = ['elev_2d', 'uv_2d']
@@ -119,22 +105,19 @@ while mn < np.ceil(T / (dt * rm)):
     solver_obj.next_export_t = solver_obj.i_export * options.simulation_export_time
     solver_obj.iteration = int(np.ceil(solver_obj.next_export_t / dt))
     solver_obj.simulation_time = solver_obj.iteration * dt
-    solver_obj.next_export_t += options.simulation_export_time
+    solver_obj.next_export_t += options.simulation_export_time  # For next export
     for e in solver_obj.exporters.values():
         e.set_next_export_ix(solver_obj.i_export)
 
     # Time integrate and print
     solver_obj.iterate()
-    nEle, nVer = adap.meshStats(mesh)
+    nEle = msh.meshStats(mesh)[0]
     N = [min(nEle, N[0]), max(nEle, N[1])]
     Sn += nEle
     mn += 1
-    print("""\n************************** Adaption step %d ****************************
-Percent complete  : %4.1f%%    Elapsed time : %4.2fs (This step : %4.2fs)
-#Elements... Current : %d  Mean : %d  Minimum : %s  Maximum : %s\n""" %
-          (mn, (100 * mn * rm * dt) / T, clock() - tic1, clock() - tic2, nEle, Sn / mn, N[0], N[1]))
+    op.printToScreen(mn, clock() - tic1, clock() - tic2, nEle, Sn, N)
 simpleAdaptTime = clock() - tic1
-print('Elapsed time for simple adaptive solver: %1.1fs (%1.2f mins) \n' % (simpleAdaptTime, simpleAdaptTime / 60))
+print('Elapsed time for adaptive solver: %1.1fs (%1.2f mins)' % (simpleAdaptTime, (simpleAdaptTime) / 60))
 
 print('********** ADJOINT BASED MESH ADAPTIVE SHALLOW WATER TEST *************\n')
 rm = 10
@@ -162,6 +145,9 @@ lu.rename("Adjoint velocity")
 le.rename("Adjoint free surface")
 adjointFile = File("plots/tests/adjointBased/adjoint.pvd")
 b = Function(W0.sub(1), name="Bathymetry").assign(0.1)
+coeff = Constant(0.)
+switch = True
+dirName = 'plots/tests/adjointBased'
 
 # Establish (smoothened) indicator function for adjoint equations
 x1 = 0.
@@ -176,26 +162,30 @@ w, xi = TestFunctions(W0)
 lu, le = split(lam)
 lu_, le_ = split(lam_)
 L = ((le - le_) * xi + inner(lu - lu_, w)
-     - Dt * g * inner(0.5 * (lu + lu_), grad(xi)) - f * xi
-     + Dt * (b * inner(grad(0.5 * (le + le_)), w) + 0.5 * (le + le_) * inner(grad(b), w))) * dx
+     - Constant(dt) * g * inner(0.5 * (lu + lu_), grad(xi)) - coeff * f * xi
+     + Constant(dt) * (b * inner(grad(0.5 * (le + le_)), w) + 0.5 * (le + le_) * inner(grad(b), w))) * dx
 adjointProblem = NonlinearVariationalProblem(L, lam)
 adjointSolver = NonlinearVariationalSolver(adjointProblem, solver_parameters=params)
 lu, le = lam.split()
 lu_, le_ = lam_.split()
 
-print("Starting adjoint run...")
+print('\nStarting fixed resolution adjoint run...')
 while mn > 0:
     print("t = %5.2fs" % t)
+
+    # Modify forcing term
+    if (t < op.Ts + 1.5 * dt) & switch:
+        coeff.assign(0.5)
+    elif (t < op.Ts + 0.5 * dt) & switch:
+        switch = False
+        coeff.assign(0.)
 
     # Solve the problem, update variables and dump to vtu and HDF5
     if mn != int(T / dt):
         adjointSolver.solve()
         lam_.assign(lam)
     adjointFile.write(lu, le, time=t)
-    with DumbCheckpoint("plots/tests/adjointBased/hdf5/adjoint_" + stor.indexString(mn), mode=FILE_CREATE) as chk:
-        chk.store(lu)
-        chk.store(le)
-        chk.close()
+    stor.saveToDisk(lu, le, dirName, stor.indexString(mn))
     t -= dt
     mn -= 1
 assert(mn == 0)
@@ -205,83 +195,57 @@ print('Elapsed time for fixed mesh adjoint solver: %1.1fs (%1.2f mins) \n' % (ad
 # Set up forward problem
 iStart = int(op.Ts / (dt * rm))         # Index corresponding to tStart
 iEnd = int(np.ceil(T / (dt * rm)))      # Index corresponding to tEnd
-dirName = 'plots/tests/adjointBased'
 
-print("Starting forward run...")
+# Approximate isotropic metric at boundaries of initial mesh using circumradius
+h = Function(W0.sub(1)).interpolate(CellSize(mesh0))
+
+print('\nStarting mesh adaptive forward run...')
 while mn < iEnd:
     tic2 = clock()
-
-    # Enforce initial conditions on discontinuous space / load variables from disk
-    mixedSpace = VectorFunctionSpace(mesh, op.space1, op.degree1) * FunctionSpace(mesh, op.space2, op.degree2)
-    uv_2d = Function(mixedSpace.sub(0))
-    elev_2d = Function(mixedSpace.sub(1))
     index = mn * int(rm / ndump)
-    indexStr = stor.indexString(index)
-    if mn == 0:
-        elev_2d.interpolate(eta0)
-        uv_2d.interpolate(Expression((0, 0)))
-    else:
-        with DumbCheckpoint(dirName + 'hdf5/Elevation2d_' + indexStr, mode=FILE_READ) as el:
-            el.load(elev_2d, name='elev_2d')
-            el.close()
-        with DumbCheckpoint(dirName + 'hdf5/Velocity2d_' + indexStr, mode=FILE_READ) as ve:
-            ve.load(uv_2d, name='uv_2d')
-            ve.close()
-
-    # Create functions to hold inner product and significance data
-    spaceMatch = (op.space1 == op.space2) & (op.degree1 == op.degree2)
-    W = mixedSpace.sub(1) if spaceMatch else FunctionSpace(mesh, op.space1, op.degree1)
-    significance = Function(W, name='Significant regions')
-
-    for j in range(max(mn, iStart), iEnd):
-        # Load adjoint data and interpolate onto current mesh
-        with DumbCheckpoint(dirName + 'hdf5/adjoint_' + stor.indexString(mn), mode=FILE_READ) as chk:
-            lu = Function(W0.sub(0), name='Adjoint velocity')
-            le = Function(W0.sub(1), name='Adjoint free surface')
-            chk.load(lu)
-            chk.load(le)
-            chk.close()
+    i0 = max(mn, iStart)
+    elev_2d, uv_2d = op.loadFromDisk(mesh, index, dirName, elev0=eta0)  # Enforce ICs / load variables from disk
+    Wcomp = FunctionSpace(mesh, "CG", 1)                                # Computational space to match metric P1 space
+    if not basic:
+        hk = Function(Wcomp).interpolate(CellSize(mesh))                # Current sizes of mesh elements
+    if mn != 0:
+        print('#### Interpolating adjoint data...')
+        elev_2d_, uv_2d_ = op.loadFromDisk(mesh, index - 1, dirName, elev0=eta0)    # Load saved forward data
+    for j in range(i0, iEnd):
+        le, lu = op.loadFromDisk(mesh0, j, dirName, adjoint=True)                   # Load saved adjoint data
         if mn != 0:
-            print('#### Interpolating adjoint data...')
-            print('    #### Step %d / %d' % (j, iEnd - max(mn, iStart)))
-            lu, le = inte.interp(mesh, lu, le)
-
-        # Estimate error and extract (pointwise) maximal values
-        rho = err.basicErrorEstimator(uv_2d, lu, elev_2d if spaceMatch else Function(W).interpolate(elev_2d),
-                                      le if spaceMatch else Function(W).interpolate(le)).dat.data
-        if j == 0:
-            significance.dat.data[:] = rho
+            print('    #### Step %d / %d' % (j + 1 - i0, iEnd - i0))
+            lu, le = inte.interp(mesh, lu, le)                                      # Interpolate onto current mesh
+        rho = err.basicErrorEstimator(uv_2d, lu, elev_2d, le) if (basic or mn == 0) else \
+            err.explicitErrorEstimator(uv_2d_, uv_2d, elev_2d_, elev_2d, lu, le, b, dt, hk)     # Estimate error
+        if j == i0:
+            errEst = Function(Wcomp).assign(rho)
         else:
-            for k in range(len(rho)):
-                if np.abs(rho[k]) > np.abs(significance.dat.data[k]):
-                    significance.dat.data[k] = rho[k]
+            errEst = adap.pointwiseMax(errEst, rho)     # Extract (pointwise) maximal values
+    errEst = assemble(sqrt(errEst * errEst))            # Take modulus (error estimator must be strictly positive)
+    errEst.rename("Local error indicators")             # TODO: how to use the ufl function `abs`?
 
-    # Interpolate initial mesh size onto new mesh and build associated boundary metric
-    V = TensorFunctionSpace(mesh, 'CG', 1)
-    M_ = adap.isotropicMetric(V, inte.interp(mesh, h)[0], bdy=True, op=op)
-
-    # Generate metric associated with significant data, gradate it, adapt mesh and interpolate variables
-    H = Function(V)
-    H = adap.constructHessian(mesh, V, elev_2d, op=op)
+    V = TensorFunctionSpace(mesh, "CG", 1)
+    M_ = adap.isotropicMetric(V, inte.interp(mesh, h)[0], bdy=True, op=op)  # Initial boundary metric
+    H = adap.constructHessian(mesh, V, elev_2d, op=op)                      # Construct Hessian
     for k in range(mesh.topology.num_vertices()):
-        H.dat.data[k] *= significance.dat.data[k]
-    M = adap.computeSteadyMetric(mesh, V, H, elev_2d, nVerT=nVerT, op=op)
-    M = adap.metricIntersection(mesh, V, M, M_, bdy=True)
-    adap.metricGradation(mesh, M, op.beta)
-    mesh = AnisotropicAdaptation(mesh, M).adapted_mesh
+        H.dat.data[k] *= errEst.dat.data[k]                                 # Scale by error estimate
+    M = adap.computeSteadyMetric(mesh, V, H, elev_2d, nVerT=nVerT, op=op)   # Generate metric
+    M = adap.metricIntersection(mesh, V, M, M_, bdy=True)                   # Intersect with initial bdy metric
+    adap.metricGradation(mesh, M, op.beta, iso=op.iso)                      # Gradate to 'smoothen' metric
+    mesh = AnisotropicAdaptation(mesh, M).adapted_mesh                      # Adapt mesh
     elev_2d, uv_2d, b = inte.interp(mesh, elev_2d, uv_2d, b)
 
-    # Get solver parameter values and construct solver, using a P1DG-P2 mixed function space
+    # Establish Thetis flow solver object
     solver_obj = solver2d.FlowSolver2d(mesh, b)
     options = solver_obj.options
     options.element_family = op.family
     options.use_nonlinear_equations = False
+    options.use_grad_depth_viscosity_term = False
     options.simulation_export_time = dt * ndump
     options.simulation_end_time = (mn + 1) * dt * rm
     options.timestepper_type = op.timestepper
     options.timestep = dt
-
-    # Specify outfile directory and HDF5 checkpointing
     options.output_directory = dirName
     options.export_diagnostics = True
     options.fields_to_export_hdf5 = ['elev_2d', 'uv_2d']
@@ -294,22 +258,17 @@ while mn < iEnd:
     solver_obj.next_export_t = solver_obj.i_export * options.simulation_export_time
     solver_obj.iteration = int(np.ceil(solver_obj.next_export_t / dt))
     solver_obj.simulation_time = solver_obj.iteration * dt
-
-    # For next export
-    solver_obj.next_export_t += options.simulation_export_time
+    solver_obj.next_export_t += options.simulation_export_time  # For next export
     for e in solver_obj.exporters.values():
         e.set_next_export_ix(solver_obj.i_export)
 
     # Time integrate and print
     solver_obj.iterate()
-    nEle, nVer = adap.meshStats(mesh)
+    nEle = msh.meshStats(mesh)[0]
     N = [min(nEle, N[0]), max(nEle, N[1])]
     Sn += nEle
     mn += 1
-    print("""\n************************** Adaption step %d ****************************
-Percent complete  : %4.1f%%    Elapsed time : %4.2fs (This step : %4.2fs)     
-#Elements... Current : %d  Mean : %d  Minimum : %s  Maximum : %s\n""" %
-          (mn, (100 * mn * rm * dt) / T, clock() - tic1, clock() - tic2, nEle, Sn / mn, N[0], N[1]))
+    op.printToScreen(mn, clock() - tic1, clock() - tic2, nEle, Sn, N)
 adjointBasedRunTime = clock() - tic1
 print('Elapsed time for adjoint based solver: %1.1fs (%1.2f mins)' % (adjointBasedRunTime, adjointBasedRunTime / 60))
 
