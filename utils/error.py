dbbe5b067970d39a38b31dfd3b04591e4d6dffcb
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-11-24 11:17:14 +0000

    Draft error computation script

--- utils/error.py
+++ utils/error.py
@@ -3,6 +3,10 @@ from firedrake import *
 import numpy as np
 import cmath
 
+from . import error
+from . import interpolation
+from . import storage
+
 
 class OutOfRangeError(ValueError):
     pass
@@ -103,3 +107,45 @@ def FourierSeriesSW(eta0, t, b, trunc=10):
             eta.dat.data[:] += \
                 ((cosTerm.dat.data + cmath.sqrt(-1) * sinTerm.dat.data) * omega * transform * cos(omega * t)).real
     return eta
+
+if __name__ == '__main__':
+    mode = input("Enter error type to compute (default fixedMesh): ") or 'fixedMesh'
+    assert mode in ('fixedMesh', 'adjointBased', 'simpleAdapt')
+    lx = 2 * np.pi
+    n = 128
+    fineMesh = SquareMesh(n, n, lx, lx)
+    if input('Hit anything except enter if Fourier expansion not yet computed. '):
+        x, y = SpatialCoordinate(fineMesh)
+        eta0 = Function(FunctionSpace(fineMesh, "CG", 2)).interpolate(
+            1e-3 * exp(- (pow(x - np.pi, 2) + pow(y - np.pi, 2))))
+        outfile = File("plots/analytic/freeSurface.pvd")
+        print("Generating Fourier series solution to linear shallow water equations...")
+        for i, t in zip(range(41), np.linspace(0., 2., 41)):
+            print("t = %.2f" % t)
+            eta = error.FourierSeriesSW(eta0, t, 0.1, trunc=10)
+            eta.rename("Fourier series free surface")
+            outfile.write(eta, time=t)
+            with DumbCheckpoint("plots/analytic/hdf5/freeSurface_" + storage.indexString(i), mode=FILE_CREATE) as chk:
+                chk.store(eta)
+                chk.close()
+
+    index = 0
+    eta = Function(FunctionSpace(fineMesh, "CG", 2), name="Analytic free surface")
+    if mode == 'fixedMesh':
+        elev_2d = Function(FunctionSpace(SquareMesh(64, 64, lx, lx), "CG", 2), name="elev_2d")
+    elif mode == 'simpleAdapt':
+        raise NotImplementedError('Need save mesh to load data.')
+    elif mode == 'adjointBased':
+        raise NotImplementedError('Need save mesh to load data.')
+    # TODO: save meshes to compute other error norms
+
+    for index, t in zip(range(41), np.linspace(0., 2., 41)):
+        indexStr = storage.indexString(index)
+        with DumbCheckpoint("plots/analytic/hdf5/freeSurface_" + indexStr, mode=FILE_READ) as exact:
+            exact.load(eta, name="Fourier series free surface")
+            exact.close()
+        with DumbCheckpoint('plots/tests/' + mode + '/hdf5/Elevation2d_' + indexStr, mode=FILE_READ) as approx:
+            approx.load(elev_2d, name="elev_2d")
+            approx.close()
+        approxn = interpolation.interp(fineMesh, elev_2d)[0]
+        print('t = %5.2fs, relative error = %8.6f' % (t, errornorm(approxn, eta) / norm(eta)))
