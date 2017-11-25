663e453d5c074edd09ba33de4146ac283d2f1a13
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-11-25 14:55:21 +0000

    Started considering discrete-adjoint SW

--- utils/forms.py
+++ utils/forms.py
@@ -1,12 +1,11 @@
 from firedrake import *
-# from firedrake_adjoint import *
 
 
-def objectiveFunctionalSW(eta, t, dt, Tstart=300., Tend=1500., x1=490e3, x2=640e3, y1=4160e3, y2=4360e3):
+def objectiveFunctionalSW(eta, t, timestep, Tstart=300., Tend=1500., x1=490e3, x2=640e3, y1=4160e3, y2=4360e3):
     """
     :param eta: free surface displacement solution.
     :param t: current time value (s).
-    :param dt: time step (s).
+    :param timestep: time step (s).
     :param Tstart: first time considered as relevant (s).
     :param Tend: last time considered as relevant (s).
     :param x1: West-most coordinate for region A (m).
@@ -26,15 +25,17 @@ def objectiveFunctionalSW(eta, t, dt, Tstart=300., Tend=1500., x1=490e3, x2=640e
 
     # Modify forcing term to 'smoothen' in time
     coeff = Constant(1.)
-    if t.dat.data < Tstart + 1.5 * dt:
+    if t.dat.data < Tstart + 1.5 * timestep:
         coeff.assign(0.5)
-    elif t.dat.data < Tstart + 0.5 * dt:
+    elif t.dat.data < Tstart + 0.5 * timestep:
         coeff.assign(0.)
 
-    return (coeff * eta * iA) * dx * dt_meas[Tstart:Tend]
+    from firedrake_adjoint import dt
+
+    return (coeff * eta * iA) * dx * dt[Tstart:Tend]
 
 
-def strongResidualSW(q, q_, b, Dt, nu=0.):
+def strongResidualSW(q, q_, b, Dt, nu=0., timestepper='CrankNicolson'):
     """
     Construct the strong residual for linear shallow water equations at the current timestep, using Crank Nicolson
     timestepping to express as a 'stationary' PDE `Lq-s=0`, where the 'source term' s depends on the data from the
@@ -47,23 +48,35 @@ def strongResidualSW(q, q_, b, Dt, nu=0.):
     :param nu: coefficient for stress term.
     :return: strong residual for shallow water equations at current timestep.
     """
-    (u, eta) = (as_vector((q[0], q[1])), q[2])
-    (u_, eta_) = (as_vector((q_[0], q_[1])), q_[2])
-    uh = 0.5 * (u + u_)
-    etah = 0.5 * (eta + eta_)
+    u, eta = q.split()
+    u_, eta_ = q_.split()
 
-    Au = u - u_ + Dt * 9.81 * grad(etah)
-    Ae = eta - eta_ + Dt * div(b * uh)
+    if timestepper == 'CrankNicolson':
+        um = 0.5 * (u + u_)
+        em = 0.5 * (eta + eta_)
+    elif timestepper == 'ImplicitEuler':
+        um = u
+        em = eta
+    elif timestepper == 'ExplicitEuler':
+        um = u_
+        em = eta_
+    else:
+        raise NotImplementedError
+
+    b = Constant(3000.)  # TODO: don't assume flat bathymetry
+
+    Au = u - u_ + Dt * 9.81 * grad(em)
+    Ae = eta - eta_ + Dt * div(b * um)
 
     if nu != 0.:
-        Au += div(nu * (grad(uh) + transpose(grad(uh))))
+        Au += div(nu * (grad(um) + transpose(grad(um))))
 
-    return as_vector([Au[i] for i in range(2)] + [Ae])
+    return Au, Ae
 
 
 # TODO: implement Galerkin Least Squares (GLS) stabilisation
 
-def weakResidualSW(q, q_, qt, b, Dt, nu=0.):
+def weakResidualSW(q, q_, qt, b, Dt, nu=0., timestepper='CrankNicolson'):
     """
     :param q: solution tuple for linear shallow water equations.
     :param q_: solution tuple for linear shallow water equations at previous timestep.
@@ -76,13 +89,23 @@ def weakResidualSW(q, q_, qt, b, Dt, nu=0.):
     (u, eta) = (as_vector((q[0], q[1])), q[2])
     (u_, eta_) = (as_vector((q_[0], q_[1])), q_[2])
     (w, xi) = (as_vector((qt[0], qt[1])), qt[2])
-    uh = 0.5 * (u + u_)
-    etah = 0.5 * (eta + eta_)
 
-    F = (inner(u - u_, w) + inner(eta - eta_, xi) + 9.81 * inner(grad(etah), w) - inner(b * uh, grad(xi))) * dx
+    if timestepper == 'CrankNicolson':
+        um = 0.5 * (u + u_)
+        em = 0.5 * (eta + eta_)
+    elif timestepper == 'ImplicitEuler':
+        um = u
+        em = eta
+    elif timestepper == 'ExplicitEuler':
+        um = u_
+        em = eta_
+    else:
+        raise NotImplementedError
+
+    F = (inner(u - u_, w) + inner(eta - eta_, xi) + 9.81 * inner(grad(em), w) - inner(b * um, grad(xi))) * dx
 
     if nu != 0.:
-        F -= nu * inner(grad(uh) + transpose(grad(uh)), grad(w))
+        F -= nu * inner(grad(um) + transpose(grad(um)), grad(w))
 
     return F
 
@@ -99,6 +122,10 @@ def strongResidualAD(c, c_, u, Dt, nu=1e-3, timestepper='CrankNicolson'):
     """
     if timestepper == 'CrankNicolson':
         cm = 0.5 * (c + c_)
+    elif timestepper == 'ImplicitEuler':
+        cm = c
+    elif timestepper == 'ExplicitEuler':
+        cm = c_
     else:
         raise NotImplementedError
     return c - c_ + Dt * inner(u, grad(cm)) - Dt * Constant(nu) * div(grad(cm))
@@ -117,6 +144,10 @@ def weakResidualAD(c, c_, ct, u, Dt, nu=1e-3, timestepper='CrankNicolson'):
     """
     if timestepper == 'CrankNicolson':
         cm = 0.5 * (c + c_)
+    elif timestepper == 'ImplicitEuler':
+        cm = c
+    elif timestepper == 'ExplicitEuler':
+        cm = c_
     else:
         raise NotImplementedError
     return ((c - c_) * ct - Dt * inner(cm * u, grad(ct)) + Dt * Constant(nu) * inner(grad(cm), grad(ct))) * dx
