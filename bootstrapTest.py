3f2f649a940a5d889096ed9ea4c543c692983e7f
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-01-02 09:12:09 +0000

    Started on bootstrapping for tsunami problems

--- bootstrapTest.py
+++ bootstrapTest.py
@@ -3,25 +3,37 @@ import matplotlib.pyplot as plt
 import utils.bootstrapping as boot
 
 
-advDiff = bool(input("\nHit anything except enter to consider advection-diffusion. "))
-filename = "advection-diffusion" if advDiff else "shallow-water"
-print("\n ***** " + filename + " *****\n")
-(Js, nEls) = boot.bootstrap(advDiff)[1:]
+problem = input("""Select from the following
+      'advection-diffusion'
+      'shallow-water'
+      'rossby-wave'
+      'firedrake-tsunami'
+      'thetis-tsunami'\n""") or 'advection-diffusion'
+(Js, nEls) = boot.bootstrap(problem)[1:]
 
 plt.gcf()
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 ylab = r'Objective functional'
-if advDiff:
+if problem == 'advection-diffusion':
     plt.title(r'Advection-diffusion test case on $[0,4]\times[0,1]$')
     ylab += ' $J(\phi)=\int_{T_{\mathrm{start}}}^{T_{\mathrm{end}}}\int_A\phi\:\mathrm{d}x\mathrm{d}t$'
-    plt.xlabel(r'\#Elements')
-else:
+elif problem == 'shallow-water':
     plt.title(r'Non-rotating shallow water test case on $[0,2]\times[0,2]$')
     ylab += ' $J(u,v,\eta)=\int_{T_{\mathrm{start}}}^{T_{\mathrm{end}}}\int_A\eta\:\mathrm{d}x\mathrm{d}t$'
-    plt.xlabel(r'\#Elements')
+elif problem == 'rossby-wave':
+    plt.title(r'Equatorial Rossby wave test case on $[-24,24]\times[-12,12]$')
+    raise NotImplementedError
+elif problem == 'firedrake-tsunami':
+    plt.title(r'Tohoku tsunami problem solved using Firedrake')
+    ylab += ' $J(u,v,\eta)=\int_{T_{\mathrm{start}}}^{T_{\mathrm{end}}}\int_A\eta\:\mathrm{d}x\mathrm{d}t$'
+elif problem == 'thetis-tsunami':
+    plt.title(r'Tohoku tsunami problem solved using Thetis')
+    ylab += ' $J(u,v,\eta)=\int_{T_{\mathrm{start}}}^{T_{\mathrm{end}}}\int_A\eta\:\mathrm{d}x\mathrm{d}t$'
+else:
+    raise ValueError("Problem not recognised.")
+plt.xlabel(r'\#Elements')
 plt.ylabel(ylab)
-# plt.semilogx([pow(2, i) for i in range(len(Js))], Js, basex=2)
-plt.semilogx(nEls, Js, basex=10)
-plt.savefig("outdata/bootstrapping/" + filename + ".pdf", bbox_inches='tight')
+plt.semilogx(nEls, Js, basex=10, marker='o')
+plt.savefig("outdata/bootstrapping/" + problem + ".pdf", bbox_inches='tight')
 plt.show()
--- utils/bootstrapping.py
+++ utils/bootstrapping.py
@@ -1,4 +1,5 @@
 from firedrake import *
+from thetis import *
 
 import numpy as np
 from time import clock
@@ -101,7 +102,95 @@ def solverSW(n, op=opt.Options(dt=0.05, Tstart=0.5, Tend=2.5, family='dg-cg',)):
     return J_trap * op.dt, nEle
 
 
-def bootstrap(advDiff, maxIter=8, tol=1e-3, slowTol=10.):
+# TODO: Rossby Wave test problem
+
+
+def solverFiredrake(coarseness, op=opt.Options()):
+
+    # Define Mesh and FunctionSpace
+    mesh, eta0, b = msh.TohokuDomain(op.coarseness)
+    nEle = msh.meshStats(mesh)[0]
+    V = VectorFunctionSpace(mesh, op.space1, op.degree1) * FunctionSpace(mesh, op.space2, op.degree2)
+
+    # Specify solver parameters
+    Dt = Constant(op.dt)
+
+    # Apply initial condition and define Functions
+    q_ = Function(V)
+    u_, eta_ = q_.split()
+    u_.interpolate(Expression([0, 0]))
+    eta_.interpolate(eta0)
+    q = Function(V)
+    q.assign(q_)
+    u, eta = q.split()
+
+    # Define variational problem and OF
+    qt = TestFunction(V)
+    forwardProblem = NonlinearVariationalProblem(form.weakResidualSW(q, q_, qt, b, Dt), q)
+    forwardSolver = NonlinearVariationalSolver(forwardProblem, solver_parameters=op.params)
+    iA = form.indicator(V.sub(1), x1=490e3, x2=640e3, y1=4160e3, y2=4360e3, smooth=True)
+
+    started = False
+    t = 0.
+    while t < op.Tend:
+
+        # Solve problem at current timestep and update variables
+        forwardSolver.solve()
+        q_.assign(q)
+
+        # Estimate OF using trapezium rule
+        step = assemble(eta * iA * dx)
+        if (t >= op.Tstart) and not started:
+            started = True
+            J_trap = step
+        elif started:
+            J_trap += 2 * step
+        if t >= op.Tend:
+            J_trap += step
+        t += op.dt
+
+    return J_trap * op.dt, nEle
+
+
+def solverThetis(coarseness, op=opt.Options()):
+
+    # Get Mesh and initial condition and bathymetry defined thereupon
+    mesh, eta0, b = msh.TohokuDomain(op.coarseness)
+    nEle = msh.meshStats(mesh)[0]
+    V = VectorFunctionSpace(mesh, op.space1, op.degree1) * FunctionSpace(mesh, op.space2, op.degree2)
+
+    # Set up solver
+    solver_obj = solver2d.FlowSolver2d(mesh, b)
+    options = solver_obj.options
+    options.element_family = op.family
+    options.use_nonlinear_equations = False
+    options.use_grad_depth_viscosity_term = False
+    options.simulation_end_time = op.Tend
+    options.timestepper_type = op.timestepper
+    options.timestep = op.dt
+    options.no_exports = True
+    options.log_output = True
+
+    # Define OF
+    t = 0.
+    iA = form.indicator(V.sub(1), x1=490e3, x2=640e3, y1=4160e3, y2=4360e3, smooth=True)
+    J_trap = 0.
+    def getJ(elev_2d, t, J_trap):
+        step = assemble(elev_2d * iA * dx)
+        t += op.dt
+        if (t == 0.) | (op.Tend - t <= op.dt):
+            J_trap += step
+        else:
+            J_trap += 2 * step
+
+    # Apply ICs and time integrate
+    solver_obj.assign_initial_conditions(elev=eta0)
+    solver_obj.iterate(export_func=getJ(elev_2d, t, J_trap))
+
+    return J_trap * op.dt, nEle
+
+
+def bootstrap(problem='advection-diffusion', maxIter=8, tol=1e-3, slowTol=10.):
     Js = []         # Container for objective functional values
     ts = []         # Timing values
     nEls = []       # Container for element counts
@@ -110,17 +199,35 @@ def bootstrap(advDiff, maxIter=8, tol=1e-3, slowTol=10.):
     reason = 'maximum number of iterations being reached.'
     for i in range(maxIter):
         tic = clock()
-        n = pow(2, i)
-        J, nEle = solverAD(n) if advDiff else solverSW(n)
+        if problem == 'advection-diffusion':
+            n = pow(2, i)
+            J, nEle = solverAD(n)
+        elif problem == 'shallow-water':
+            n = pow(2, i)
+            J, nEle = solverSW(n)
+        elif problem == 'rossby-wave':
+            raise NotImplementedError
+        elif problem == 'firedrake-tsunami':
+            J, nEle = solverFiredrake(5-i)
+        elif problem == 'thetis-tsunami':
+            J, nEle = solverThetis(5-i)
+        else:
+            raise ValueError("Problem not recognised.")
         t = clock() - tic
         Js.append(J)
         ts.append(t)
         nEls.append(nEle)
-        toPrint = "n = %3d, nEle = %6d, J = %6.4f, run time : %5.3f, " % (n, nEle, Js[-1], t)
+        if problem in ('firedrake-tsunami', 'thetis-tsunami'):
+            toPrint = 'coarseness = %2d, ' % (5-i)
+        else:
+            toPrint = 'n = %3d, ' % n
+        toPrint += "nEle = %6d, J = %6.4f, run time : %5.3f, " % (nEle, Js[-1], t)
+        if i > 0:
+            slowdown = ts[-1] / ts[-2]
+            toPrint += "slowdown : %5.3f, " % slowdown
         if i > 1:
             diff = np.abs(np.abs(Js[-2] - Js[-3]) - np.abs(Js[-1] - Js[-2]))
-            slowdown = ts[-1]/ts[-2]
-            toPrint += "diff : %6.4f, slowdown : %5.3f" % (diff, slowdown)
+            toPrint += "diff : %6.4f" % diff
         print(toPrint)
         iOpt = i+1  # Get current iteration number
 
@@ -130,6 +237,10 @@ def bootstrap(advDiff, maxIter=8, tol=1e-3, slowTol=10.):
 
         if slowdown > slowTol:
             reason = 'run time decreasing too much.'
+            break
+
+        if (problem in ('firedrake-tsunami', 'thetis-tsunami')) & (i == 4):
+            reason = 'maximum mesh resolution reached.'
             break
 
     print("Converged to J = %.4f in %d iterations, due to %s" % (Js[-1], iOpt, reason))
