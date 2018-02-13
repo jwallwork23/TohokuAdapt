2daca4515cbfc1681df170be7a93769e208d66de
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-02-13 13:47:30 +0000

    No-slip boundary conditions in rossby-wave

--- thetis-rossby-wave.py
+++ thetis-rossby-wave.py
@@ -23,15 +23,18 @@ xy.dat.data[:, 1] -= 12.
 mesh.coordinates.assign(xy)
 x, y = SpatialCoordinate(mesh)
 
-# Set initial condition and bathymetry
+# Set initial/boundary conditions and bathymetry
 V = VectorFunctionSpace(mesh, op.space1, op.degree1) * FunctionSpace(mesh, op.space2, op.degree2)
 q_ = form.analyticHuang(V)
 uv0, elev0 = q_.split()
+BCs = {}
+BCs[1] = {'uv': Function(V.sub(0))}     # Zero velocity on South boundary
+BCs[2] = {'uv': Function(V.sub(0))}     # Zero velocity on North boundary
 b = Function(FunctionSpace(mesh, "CG", 1)).assign(1.)
 
 # Set Coriolis frequency and gravitational acceleration
-f0 = 0.     # TODO: put in options
-beta = 1.   # TODO: put in options
+f0 = 0.
+beta = 1.
 f = f0 + beta * y
 physical_constants['g_grav'] = Constant(1.)
 
@@ -43,7 +46,7 @@ dt = min(np.abs(solver_obj.compute_time_step().dat.data))
 # Solve
 options = solver_obj.options
 options.element_family = op.family
-options.use_nonlinear_equations = False
+options.use_nonlinear_equations = True
 options.use_grad_depth_viscosity_term = False
 options.coriolis_frequency = f
 options.simulation_export_time = dt * op.ndump
@@ -52,4 +55,5 @@ options.timestepper_type = op.timestepper
 options.timestep = dt
 options.output_directory = 'plots/rossby-wave'
 solver_obj.assign_initial_conditions(elev=elev0, uv=uv0)
+solver_obj.bnd_functions['shallow_water'] = BCs
 solver_obj.iterate()
