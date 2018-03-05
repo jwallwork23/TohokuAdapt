c3135f9cb609b0c1e1d9b75e9ea41a2e86e471b8
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-05 10:58:32 +0000

    More error estimators in Helmholtz test

--- helmholtzTest.py
+++ helmholtzTest.py
@@ -6,6 +6,7 @@ from time import clock
 import matplotlib.pyplot as plt
 
 import utils.adaptivity as adap
+import utils.forms as form
 import utils.interpolation as inte
 import utils.mesh as msh
 import utils.options as opt
@@ -68,23 +69,62 @@ def adaptive(mesh_HIterations=3, degree=1, approach='hessianBased', op=op):
         for cnt in range(mesh_HIterations):
             if approach == 'hessianBased':
                 M = adap.computeSteadyMetric(u_H, nVerT=nVerT, op=op)
-            elif approach == 'higherOrderResidual':
+            elif approach in ('higherOrderResidual', 'higherOrderImplicit', 'higherOrderExplicit'):
                 x, y = SpatialCoordinate(mesh_H)
                 V_oi = FunctionSpace(mesh_H, "CG", degree+1)
                 u_H_oi = Function(V_oi).interpolate(u_H)
                 f_oi = Function(V_oi).interpolate((1+8*pi*pi)*cos(2*pi*x)*cos(2*pi*y))
-                v_DG0 = TestFunction(FunctionSpace(mesh_H, "DG", 0))
-                R_oi = assemble(v_DG0 * (- div(grad(u_H_oi)) + u_H_oi - f_oi) * dx)
-                M = adap.isotropicMetric(R_oi, invert=False, nVerT=nVerT, op=op)
-            elif approach == 'refinedResidual':
+                if approach in ('higherOrderResidual', 'higherOrderExplicit'):
+                    v_DG0 = TestFunction(FunctionSpace(mesh_H, "DG", 0))
+                    R_oi = assemble(v_DG0 * (- div(grad(u_H_oi)) + u_H_oi - f_oi) * dx)
+                    if approach == 'higherOrderResidual':
+                        M = adap.isotropicMetric(R_oi, invert=False, nVerT=nVerT, op=op)
+                    else:
+                        hk = CellSize(mesh_H)
+                        resTerm = assemble(v_DG0 * hk * hk * R_oi * R_oi * dx)
+                        j_bdy = assemble(dot(v_DG0 * grad(u_H_oi), FacetNormal(mesh_H)) * ds)
+                        j_int = assemble(jump(v_DG0 * grad(u_H_oi), n=FacetNormal(mesh_H)) * dS)
+                        jumpTerm = assemble(v_DG0 * hk * (j_bdy * j_bdy + j_int * j_int) * dx)
+                        M = adap.isotropicMetric(assemble(sqrt(resTerm + jumpTerm)), invert=False, nVerT=nVerT, op=op)
+                elif approach == 'higherOrderImplicit':
+                    v_oi = TestFunction(V_oi)
+                    e = Function(V_oi)
+                    Be = (inner(grad(e), grad(v_oi)) + e * v_oi) * dx
+                    Bu = (inner(grad(u_H_oi), grad(v_oi)) + u_H_oi * v_oi) * dx
+                    L_oi = f_oi * v_oi * dx
+                    I = form.interelementTerm(v_oi * grad(u_H_oi), n=FacetNormal(mesh_H)) * dS
+                    F_oi = Be - L_oi + Bu - I
+                    solve(F_oi == 0, e)
+                    M = adap.isotropicMetric(e, invert=False, nVerT=nVerT, op=op)
+            elif approach in ('refinedResidual', 'refinedImplicit', 'refinedExplicit'):
                 mesh_h = adap.isoP2(mesh_H)
                 x, y = SpatialCoordinate(mesh_h)
                 u_h = inte.interp(mesh_h, u_H)[0]
                 V_h = FunctionSpace(mesh_h, "CG", degree)
                 f_h = Function(V_h).interpolate((1+8*pi*pi)*cos(2*pi*x)*cos(2*pi*y))
-                v_DG0 = TestFunction(FunctionSpace(mesh_h, "DG", 0))
-                R_h = assemble(v_DG0 * (- div(grad(u_h)) + u_h - f_h) * dx)
-                M = adap.isotropicMetric(inte.interp(mesh_H, R_h)[0], invert=False, nVerT=nVerT, op=op)
+                if approach in ('refinedResidual', 'refinedExplicit'):
+                    v_DG0 = TestFunction(FunctionSpace(mesh_h, "DG", 0))
+                    R_h = assemble(v_DG0 * (- div(grad(u_h)) + u_h - f_h) * dx)
+                    if approach == 'refinedResidual':
+                        M = adap.isotropicMetric(inte.interp(mesh_H, R_h)[0], invert=False, nVerT=nVerT, op=op)
+                    else:
+                        hk = CellSize(mesh_h)
+                        resTerm = assemble(v_DG0 * hk * hk * R_h * R_h * dx)
+                        j_bdy = assemble(dot(v_DG0 * grad(u_h), FacetNormal(mesh_h)) * ds)
+                        j_int = assemble(jump(v_DG0 * grad(u_h), n=FacetNormal(mesh_h)) * dS)
+                        jumpTerm = assemble(v_DG0 * hk * (j_bdy * j_bdy + j_int * j_int) * dx)
+                        eps = assemble(sqrt(resTerm + jumpTerm))
+                        M = adap.isotropicMetric(inte.interp(mesh_H, eps)[0], invert=False, nVerT=nVerT, op=op)
+                else:
+                    v_h = TestFunction(V_h)
+                    e = Function(V_h)
+                    Be = (inner(grad(e), grad(v_h)) + e * v_h) * dx
+                    Bu = (inner(grad(u_h), grad(v_h)) + u_h * v_h) * dx
+                    L_h = f_h * v_h * dx
+                    I = form.interelementTerm(v_h * grad(u_h), n=FacetNormal(mesh_h)) * dS
+                    F_h = Be - L_h + Bu - I
+                    solve(F_h == 0, e)
+                    M = adap.isotropicMetric(inte.interp(mesh_H, e)[0], invert=False, nVerT=nVerT, op=op)
             else:
                 raise NotImplementedError
             if op.gradate:
@@ -92,7 +132,7 @@ def adaptive(mesh_HIterations=3, degree=1, approach='hessianBased', op=op):
             mesh_H = AnisotropicAdaptation(mesh_H, M).adapted_mesh
             f = inte.interp(mesh_H, f)[0]
             u_H, u, f = helmholtzSolve(mesh_H, degree, f)
-            err = la.norm(u_H.dat.data - u.dat.data)
+            err = errornorm(u, u_H)
         nEle = msh.meshStats(mesh_H)[0]
         tic = clock()-tic
         print("Initial mesh_H %d   Error: %.4f    #Elements: %d     Timing: %.2fs" % (i, err, nEle, tic))
@@ -166,7 +206,8 @@ if __name__ == '__main__':
             nEls.append(nEle)
             times.append(tic)
     elif mode == 'approach':
-        S = ('hessianBased', 'higherOrderResidual', 'refinedResidual')
+        S = ('hessianBased', 'higherOrderExplicit', 'refinedExplicit', 'higherOrderResidual', 'refinedResidual',
+             'higherOrderImplicit', 'refinedImplicit')
         for approach in S:
             print("\nTesting use of error estimator %s\n" % approach)
             err, nEle, tic = adaptive(approach=approach, op=op)
