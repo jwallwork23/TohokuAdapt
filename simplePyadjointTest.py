02299137612b75d29164d4101b3f75e703ac5b98
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-03-26 15:55:44 +0100

    Get adjoint soln variables for Burgers eqn

--- simplePyadjointTest.py
+++ simplePyadjointTest.py
@@ -1,72 +1,71 @@
 from firedrake import *
 from firedrake_adjoint import *
-from fenics_adjoint.solving import SolveBlock
+from fenics_adjoint.solving import SolveBlock   # Need checkout Sebastians `linear-solver` branch of pyadjoint
 
-import utils.forms as form
+import numpy as np
 
+n = 30
+mesh = UnitSquareMesh(n, n)
+x, y = SpatialCoordinate(mesh)
+V = VectorFunctionSpace(mesh, "CG", 2)
+V0 = FunctionSpace(mesh, "CG", 2)
 
-forwardFile = File('plots/pyadjoint_test/forward.pvd')
-adjointFile = File('plots/pyadjoint_test/adjoint.pvd')
+u0 = Function(V0).interpolate(sin(2*pi*x))
+u1 = Function(V0).interpolate(cos(2*pi*y))
+u = Function(V).interpolate(as_vector([u0, u1]))    # Can't currently annotate Expression objects in firedrake_adjoint
+control = Control(u)
 
-# Load Mesh(es)
-n = 8
-mesh_H = SquareMesh(4 * n, n, 4, 1)
-x, y = SpatialCoordinate(mesh_H)
-P1 = FunctionSpace(mesh_H, "CG", 1)
-w = Constant([1, 0])
-c_ = Function(P1).interpolate(exp(- (pow(x - 0.5, 2) + pow(y - 0.5, 2)) / 0.04))
-c = TrialFunction(P1)
+u_next = Function(V)
+v = TestFunction(V)
 
-# Specify physical and solver parameters
-dt = 0.05
-Dt = Constant(dt)
-Tstart = 0.
-Tend = 2.4
-nu = Constant(1e-3)
+nu = Constant(0.0001)
+dt = Constant(0.01)
+F = (inner((u_next - u)/dt, v)
+     + inner(grad(u_next)*u_next, v)
+     + nu*inner(grad(u_next), grad(v)))*dx
+bc = DirichletBC(V, (0.0, 0.0), "on_boundary")
 
-# Define adjoint variable and indicator function
-dual = Function(P1)
-dual.rename("Adjoint")
-def indicator(P1):
-    k = Function(P1, name='Region of interest', annotate=False)
-    k.interpolate(Expression('(x[0] > 2.5) & (x[0] < 3.5) & (x[1] > 0.1) & (x[1] < 0.9) ? 1. : 0.'), annotate=False)
-    return k
-k = form.indicator(P1)
+forwardFile = File("plots/pyadjoint_test/burgers.pvd")
+forwardFile.write(u)
 
-# Define variational problem
-ct = TestFunction(P1)
-a = (c * ct / Dt) * dx
-L = (c_ * ct / Dt - inner(grad(c_), w * ct) - nu * inner(grad(c_), grad(ct))) * dx
-c = Function(P1)
-prob = LinearVariationalProblem(a, L, c)
-solv = LinearVariationalSolver(prob)
+t = 0.0
+cnt = 0
+end = 0.1
+Jtemp = assemble(inner(u,u)*dx)
+Jlist = [Jtemp]
+while (t <= end):
+    solve(F == 0, u_next, bc)
+    u.assign(u_next)
+    t += float(dt)
+    cnt += 1
 
-t = 0.
-forwardFile.write(c_, time=t)
-# Jfuncs = [assemble(inner(k, c_) * dx)]
-J = assemble(k * c_ * dx)
-while t < Tend + dt:
-    solv.solve()
-    c_.assign(c)
-    # Jfuncs.append(assemble(inner(k, c_) * dx))  # Update OF
-    forwardFile.write(c_, time=t)
-    print('t = %.2fs' % t)
-    t += dt
-t -= dt
+    Jtemp = assemble(inner(u, u)*dx)
+    Jlist.append(Jtemp)
 
-# # Establish OF
-# J = 0
-# for i in range(1, len(Jfuncs)):
-#     J += 0.5*(Jfuncs[i-1] + Jfuncs[i])*dt
+    print("t = ", np.round(t, 3))
+    forwardFile.write(u)
+t -= float(dt)
 
-dJdnu = compute_gradient(J, Control(nu))
+J = 0
+for i in range(1, len(Jlist)):
+    J += 0.5*(Jlist[i-1] + Jlist[i])*float(dt)
+# def indicator(P1):
+#     k = Function(P1, name='Region of interest', annotate=False)
+#     k.interpolate(Expression('(x[0] > 2.5) & (x[0] < 3.5) & (x[1] > 0.1) & (x[1] < 0.9) ? 1. : 0.'), annotate=False)
+#     return k
+# k = form.indicator(V, mode='advection-diffusion')
+
+dual = Function(V, name="Adjoint")
+dJdu, dJdnu = compute_gradient(J, [control, Control(nu)])
+adjointFile = File("plots/pyadjoint_test/adjoint.pvd")
+
 tape = get_working_tape()
-tape.visualise()
+# tape.visualise()
 solve_blocks = [block for block in tape._blocks if isinstance(block, SolveBlock)]
 
 for i in range(len(solve_blocks)-1, -1, -1):
     dual.assign(solve_blocks[i].adj_sol)
     adjointFile.write(dual, time=t)
-    t -= dt
-
-File('plots/pyadjoint_test/gradient.pvd').write(dJdnu)
+    print("t = ", np.round(t, 3))
+    t -= float(dt)
+print("Gradient = ", float(dJdnu.dat.data))
