e5e782be68eb91f2cc1c985f6b28198d7a81f983
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-04-04 09:59:05 +0100

    Updated imports for model verification

--- modelVerification.py
+++ modelVerification.py
@@ -4,25 +4,25 @@ import numpy as np
 from time import clock
 import datetime
 
-import utils.conversion as conv
-import utils.error as err
-import utils.mesh as msh
-import utils.options as opt
+from utils.conversion import get_latitude
+from utils.error import TohokuCallback, P02Callback, P06Callback, gaugeTV
+from utils.mesh import problemDomain
+from utils.options import Options
 
 
 now = datetime.datetime.now()
 date = str(now.day) + '-' + str(now.month) + '-' + str(now.year % 2000)
 
 
-def solverSW(startRes, op=opt.Options()):
-    mesh, eta0, b = msh.TohokuDomain(startRes, wd=op.wd)[:3]
+def solverSW(startRes, op=Options()):
+    mesh, eta0, b = problemDomain(startRes, wd=op.wd)[:3]
 
     # Get Coriolis frequency
     f = Function(FunctionSpace(mesh, 'CG', 1))
     if op.rotational:
         Omega = 7.291e-5
         for i, v in zip(range(len(mesh.coordinates.dat.data)), mesh.coordinates.dat.data):
-            f.dat.data[i] = 2 * Omega * np.sin(np.radians(conv.get_latitude(v[0], v[1], 54, northern=True)))
+            f.dat.data[i] = 2 * Omega * np.sin(np.radians(get_latitude(v[0], v[1], 54, northern=True)))
 
     # Get solver parameter values and construct solver
     solver_obj = solver2d.FlowSolver2d(mesh, b)
@@ -43,11 +43,11 @@ def solverSW(startRes, op=opt.Options()):
 
     # Apply ICs and establish Callbacks
     solver_obj.assign_initial_conditions(elev=eta0)
-    cb1 = err.TohokuCallback(solver_obj)        # Objective functional computation error
+    cb1 = TohokuCallback(solver_obj)        # Objective functional computation error
     solver_obj.add_callback(cb1, 'timestep')
-    cb2 = err.P02Callback(solver_obj)           # Gauge timeseries error P02
+    cb2 = P02Callback(solver_obj)           # Gauge timeseries error P02
     solver_obj.add_callback(cb2, 'timestep')
-    cb3 = err.P06Callback(solver_obj)           # Gauge timeseries error P06
+    cb3 = P06Callback(solver_obj)           # Gauge timeseries error P06
     solver_obj.add_callback(cb3, 'timestep')
 
     # Run simulation and extract values from Callbacks
@@ -69,9 +69,8 @@ if __name__ == '__main__':
     parser.add_argument("-l", help="Use linearised equations")
     parser.add_argument("-w", help="Use wetting and drying")
     args = parser.parse_args()
-    op = opt.Options(family='dg-dg',
-                     wd=True if args.w else False,
-                     ndump=10)
+    op = Options(family='dg-dg',
+                 wd=True if args.w else False)
     op.nonlinear = False if args.l else True
     op.rotational = True if args.r else False
     filename = 'outdata/outputs/modelVerification/nonlinear=' + str(op.nonlinear) + '_'
@@ -85,8 +84,8 @@ if __name__ == '__main__':
         J_h, gP02, gP06, timing = solverSW(k, op=op)
         gaugeFileP02.writelines(["%s," % val for val in gP02])
         gaugeFileP06.writelines(["%s," % val for val in gP06])
-        totalVarP02 = err.gaugeTV("P02")
-        totalVarP06 = err.gaugeTV("P06")
+        totalVarP02 = gaugeTV("P02")
+        totalVarP06 = gaugeTV("P06")
         errorfile.write('%d, %.4e, %.4e, %.4e, %.1f\n' % (k, J_h, totalVarP02, totalVarP06, timing))
         print("\nRun %d... J_h: %.4e TV P02: %.3f, TV P06: %.3f, time: %.1f\n"
               % (k, J_h, totalVarP02, totalVarP06, timing))
