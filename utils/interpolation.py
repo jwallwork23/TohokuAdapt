a5d1d3ebddd3758ee24beb94eaeaa45ab3bed031
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-04-05 15:38:42 +0100

    Neatening of interpolation script

--- utils/interpolation.py
+++ utils/interpolation.py
@@ -1,7 +1,5 @@
 from thetis import *
 
-from .conversion import OutOfRangeError
-
 
 __all__ = ["interp", "mixedPairInterp"]
 
@@ -17,60 +15,54 @@ def interp(mesh, *fields):
     :arg fields: tuple of functions defined on the old mesh that one wants to transfer
     :return: interpolated fields.
     """
-    dim = mesh._topological_dimension
     try:
-        assert dim == 2
+        assert mesh._topological_dimension == 2
     except:
         raise NotImplementedError('3D implementation not yet considered.')
     fields_new = ()
     for f in fields:
-        V_new = FunctionSpace(mesh, f.function_space().ufl_element())
-        f_new = Function(V_new)
+        element = f.function_space().ufl_element()
+        family = element.family()
+        degree = element.degree()
+        f_new = Function(FunctionSpace(mesh, element))
         notInDomain = []
-        if f.ufl_element().family() == 'Lagrange' and f.ufl_element().degree() == 1:
-            coords = mesh.coordinates.dat.data  # Vertex/node coords
-        elif f.ufl_element().family() == 'Lagrange':
-            degree = f.ufl_element().degree()
-            C = VectorFunctionSpace(mesh, 'CG', degree)
-            interp_coordinates = Function(C)
-            interp_coordinates.interpolate(mesh.coordinates)
-            coords = interp_coordinates.dat.data  # Node coords (NOT just vertices)
-        elif f.ufl_element().family() == 'Discontinuous Lagrange':
-            degree = f.ufl_element().degree()
-            C = VectorFunctionSpace(mesh, 'DG', degree)
-            interp_coordinates = Function(C)
-            interp_coordinates.interpolate(mesh.coordinates)
-            coords = interp_coordinates.dat.data
+        if family == 'Lagrange' and degree == 1:
+            coords = mesh.coordinates.dat.data      # Vertex/node coords
+        elif family in ('Lagrange', 'Discontinuous Lagrange'):
+            C = VectorFunctionSpace(mesh, family, degree)
+            interp_coordinates = Function(C).interpolate(mesh.coordinates)
+            coords = interp_coordinates.dat.data    # Node coords (NOT just vertices)
         else:
             raise NotImplementedError('Interpolation not currently supported on requested field type.')
-        try:
-            f_new.dat.data[:] = f.at(coords)
-        # Establish which vertices fall outside the domain  # TODO: Figure out how to do this in a less hacky way
-        except PointNotInDomainError:
-            for x in range(len(coords)):
-                try:
-                    val = f.at(coords[x])
-                except PointNotInDomainError:
-                    val = 0.
-                    notInDomain.append(x)
-                finally:
-                    f_new.dat.data[x] = val
-        eps = 1e-6  # Tolerance to be increased
+
+        # Establish which vertices fall outside the domain    TODO: Figure out how to do this in a less hacky way
+        for x in range(len(coords)):        # This looping is unnecessary in some cases
+            try:
+                val = f.at(coords[x])
+            except PointNotInDomainError:
+                # print("#### DEBUG: offending coordinates = ", coords[x])
+                val = None
+                notInDomain.append(x)
+            finally:
+                f_new.dat.data[x] = val
+        eps = 1e-6                          # Tolerance to be increased
         while len(notInDomain) > 0:
             eps *= 10
             for x in notInDomain:
                 try:
                     val = f.at(coords[x], tolerance=eps)
                 except PointNotInDomainError:
-                    val = 0.
+                    # print("#### DEBUG: offending coordinates = ", coords[x])
+                    val = None
                 finally:
                     f_new.dat.data[x] = val
                     notInDomain.remove(x)
-            if eps >= 1e8:
-                raise OutOfRangeError('Playing with epsilons failed. Abort.')
+            if eps >= 1e8:                                  # TODO: This value seems a bit high
+                raise PointNotInDomainError('Playing with epsilons failed. Abort.')
         fields_new += (f_new,)
+
     if len(fields_new) == 1:
-        return fields_new[0]    # TODO: Test this
+        return fields_new[0]
     else:
         return fields_new
 
