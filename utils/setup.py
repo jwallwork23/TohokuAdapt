679a4d8ba3bd8a251999c765d781a69b75ebe6fe
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-05-03 11:36:36 +0100

    Corrected indexing translation error

--- utils/setup.py
+++ utils/setup.py
@@ -282,50 +282,50 @@ class RossbyWaveSolution:
         eta = np.zeros(28)
 
         #  Hermite series coefficients for u:
-        u[1] = 1.789276   # TODO: Should this start at zero?
-        u[3] = 0.1164146
-        u[5] = -0.3266961e-3
-        u[7] = -0.1274022e-2
-        u[9] = 0.4762876e-4
-        u[11] = -0.1120652e-5
-        u[13] = 0.1996333e-7
-        u[15] = -0.2891698e-9
-        u[17] = 0.3543594e-11
-        u[19] = -0.3770130e-13
-        u[21] = 0.3547600e-15
-        u[23] = -0.2994113e-17
-        u[25] = 0.2291658e-19
-        u[27] = -0.1178252e-21
+        u[0] = 1.789276
+        u[2] = 0.1164146
+        u[4] = -0.3266961e-3
+        u[6] = -0.1274022e-2
+        u[8] = 0.4762876e-4
+        u[10] = -0.1120652e-5
+        u[12] = 0.1996333e-7
+        u[14] = -0.2891698e-9
+        u[16] = 0.3543594e-11
+        u[18] = -0.3770130e-13
+        u[20] = 0.3547600e-15
+        u[22] = -0.2994113e-17
+        u[24] = 0.2291658e-19
+        u[26] = -0.1178252e-21
 
         #  Hermite series coefficients for v:
-        v[4] = -0.6697824e-1  # TODO: Should this start at three?
-        v[6] = -0.2266569e-2
-        v[8] = 0.9228703e-4
-        v[10] = -0.1954691e-5
-        v[12] = 0.2925271e-7
-        v[14] = -0.3332983e-9
-        v[16] = 0.2916586e-11
-        v[18] = -0.1824357e-13
-        v[20] = 0.4920951e-16
-        v[22] = 0.6302640e-18
-        v[24] = -0.1289167e-19
-        v[26] = 0.1471189e-21
+        v[3] = -0.6697824e-1
+        v[5] = -0.2266569e-2
+        v[7] = 0.9228703e-4
+        v[9] = -0.1954691e-5
+        v[11] = 0.2925271e-7
+        v[13] = -0.3332983e-9
+        v[15] = 0.2916586e-11
+        v[17] = -0.1824357e-13
+        v[19] = 0.4920951e-16
+        v[21] = 0.6302640e-18
+        v[23] = -0.1289167e-19
+        v[25] = 0.1471189e-21
 
         #  Hermite series coefficients for eta:
-        eta[1] = -3.071430  # TODO: Should this start at zero?
-        eta[3] = -0.3508384e-1
-        eta[5] = -0.1861060e-1
-        eta[7] = -0.2496364e-3
-        eta[9] = 0.1639537e-4
-        eta[11] = -0.4410177e-6
-        eta[13] = 0.8354759e-9
-        eta[15] = -0.1254222e-9
-        eta[17] = 0.1573519e-11
-        eta[19] = -0.1702300e-13
-        eta[21] = 0.1621976e-15
-        eta[23] = -0.1382304e-17
-        eta[25] = 0.1066277e-19
-        eta[27] = -0.1178252e-21
+        eta[0] = -3.071430
+        eta[2] = -0.3508384e-1
+        eta[4] = -0.1861060e-1
+        eta[6] = -0.2496364e-3
+        eta[8] = 0.1639537e-4
+        eta[10] = -0.4410177e-6
+        eta[12] = 0.8354759e-9
+        eta[14] = -0.1254222e-9
+        eta[16] = 0.1573519e-11
+        eta[18] = -0.1702300e-13
+        eta[20] = 0.1621976e-15
+        eta[22] = -0.1382304e-17
+        eta[24] = 0.1066277e-19
+        eta[26] = -0.1178252e-21
 
         return {'u': u, 'v': v, 'eta': eta}
 
@@ -403,7 +403,11 @@ class RossbyWaveSolution:
         return terms
 
     def integrate(self):
-        outFile = File("plots/rossby-wave/analytic/analytic.pvd")
+        """
+        :return: list containing time integrand values at each timestep.
+        """
+        if self.op.plotpvd:
+            outFile = File("plots/rossby-wave/analytic/analytic.pvd")
         t = 0.
         cnt = 0
         vals = []
@@ -420,7 +424,7 @@ class RossbyWaveSolution:
             if t > self.op.Tstart - 0.5 * self.op.dt:  # Slightly smoothed transition
                 kt.assign(1. if t > self.op.Tstart + 0.5 * self.op.dt else 0.5)
             vals.append(assemble(kt * inner(ks, q) * dx))
-            if self.op.plotpvd and cnt % self.op.ndump:
+            if self.op.plotpvd and cnt % self.op.cntT / 10:
                 u, eta = q.split()
                 u.rename("Depth averaged velocity")
                 eta.rename("Elevation")
