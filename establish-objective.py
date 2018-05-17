22f59883ffa3b11477a7149d34663535507b8a43
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-05-17 16:47:48 +0100

    establish-objective untested but more or less there

--- establish-objective.py
+++ establish-objective.py
@@ -1,9 +1,8 @@
 from thetis import *
 from firedrake.petsc import PETSc
 
-from time import clock
-
 from utils.adaptivity import isoP2
+from utils.callbacks import SWCallback
 from utils.interpolation import interp
 from utils.setup import problemDomain
 from utils.options import Options
@@ -35,38 +34,17 @@ def getObjective(mesh, b, op=Options()):
     solver_obj.assign_initial_conditions(elev=eta0, uv=u0)
     cb1 = SWCallback(solver_obj)
     cb1.op = op
-    if op.mode == 'tohoku':
-        cb3 = P02Callback(solver_obj)
-        cb4 = P06Callback(solver_obj)
-        solver_obj.add_callback(cb3, 'timestep')
-        solver_obj.add_callback(cb4, 'timestep')
     solver_obj.add_callback(cb1, 'timestep')
     solver_obj.bnd_functions['shallow_water'] = BCs
 
     # Solve and extract timeseries / functionals
     quantities = {}
-    solverTimer = clock()
     solver_obj.iterate()
-    solverTimer = clock() - solverTimer
     quantities['J_h'] = cb1.quadrature()  # Evaluate objective functional
     quantities['Integrand'] = cb1.getVals()
-    if op.mode == 'tohoku':
-        quantities['TV P02'] = cb3.totalVariation()
-        quantities['TV P06'] = cb4.totalVariation()
-        quantities['P02'] = cb3.getVals()
-        quantities['P06'] = cb4.getVals()
-
-    # Measure error using metrics, as in Huang et al.
-    if op.mode == 'rossby-wave':
-        peak, distance = peakAndDistance(solver_obj.fields.solution_2d.split()[1], op=op)
-        quantities['peak'] = peak / peak_a
-        quantities['dist'] = distance / distance_a
-        quantities['spd'] = distance / (op.Tend * 0.4)
 
     # Output mesh statistics and solver times
     quantities['meanElements'] = mesh.num_cells()
-    quantities['solverTimer'] = solverTimer
-    quantities['adaptSolveTimer'] = 0.
 
     return quantities
 
@@ -74,12 +52,22 @@ def getObjective(mesh, b, op=Options()):
 if __name__ == "__main__":
 
     op = Options(mode='tohoku')
-
     mesh, b = problemDomain(0, op=op)[0::3]
 
-    OF = [getObjective(mesh, b, op)]
+    q = getObjective(mesh, b, op)
+    OF = [q['J_h']]
+    nEls = [q['meanElements']]
+    PETSc.Sys.Print("   Objective value %s" % OF[0])
+    PETSc.Sys.Print("   Element count %s" % nEls[0])
 
     for i in range(5):
         mesh = isoP2(mesh)      # Hierarchical refinement
         b = interp(mesh, b)
-        OF.append(getObjective(mesh, b, op))
+        q = getObjective(mesh, b, op)
+        OF.append(q['J_h'])
+        nEls.append(q['meanElements'])
+        PETSc.Sys.Print("   Objective value %s" % OF[-1])
+        PETSc.Sys.Print("   Element count %s" % nEls[-1])
+
+    PETSc.Sys.Print("Objective values %s" % OF)
+    PETSc.Sys.Print("Element counts %s" % nEls)
