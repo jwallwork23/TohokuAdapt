1013fcfce3948981fa546484331f9ff92d781f97
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-05-20 11:01:15 +0100

    Forgot to move to next line

--- generate-coarse-bathy.py
+++ generate-coarse-bathy.py
@@ -9,9 +9,11 @@ bathyfile = open("resources/bathymetry/array.txt", "w")
 op = Options(mode='tohoku')
 mesh, b, hierarchy = problemDomain(0, hierarchy=True, op=op)[0::3]
 bathyfile.writelines(["%s," % val for val in b.dat.data])
+bathyfile.write('\n')
 
 for i in range(1, 5):
     mesh = hierarchy.__getitem__(i)
     b = interp(mesh, b)
     bathyfile.writelines(["%s," % val for val in b.dat.data])
+    bathyfile.write('\n')
 bathyfile.close()
