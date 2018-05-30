ab2668215904e2dbbf289a124028b0999481373e
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-05-30 18:36:47 +0200

    Restructuring of files to avoid unnecessary pyadjoint calls

--- advection-diffusion.py
+++ advection-diffusion.py
@@ -1,29 +1,49 @@
-import argparse
+from firedrake import *
 
-from solvers.apriori import fixedMesh, hessianBased
+from time import clock
+
+from utils.adaptivity import *
+from utils.interpolation import interp
+from utils.misc import indicator, indexString
+from utils.options import Options
+
+from solvers.apriori import fixedMeshAD, hessianBasedAD
 from utils.options import Options
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-a", help="Choose adaptive approach from {'hessianBased', 'DWP', 'DWR'} (default 'fixedMesh')")
-parser.add_argument("-low", help="Lower bound for index range")
-parser.add_argument("-high", help="Upper bound for index range")
-parser.add_argument("-nAdapt")
-parser.add_argument("-o", help="Output data")
-args = parser.parse_args()
+def DWP():
+    raise NotImplementedError
 
-op = Options(mode="advection-diffusion",
-             approach='fixedMesh' if args.a is None else args.a,
-             plotpvd=True if bool(args.o) else False,
-             nAdapt= int(args.nAdapt) if args.nAdapt is not None else 1)
 
-resolutions = range(0 if args.low is None else int(args.low), 6 if args.high is None else int(args.high))
-solvers = {'fixedMesh': fixedMesh, 'hessianBased': hessianBased}
+def DWR():
+    raise NotImplementedError
 
-errorFile = open('outdata/advection-diffusion/' + op.approach + '.txt', 'w+')
-for i in resolutions:
-    q = solvers[op.approach](pow(2, i), op=op)
-    print("Run %d: Mean element count: %6d Objective: %.4e Timing %.1fs"
-          % (i, q['meanElements'], q['J_h'], q['solverTimer']))
-    errorFile.write('%d, %.4e\n' % (q['meanElements'], q['J_h']))
-errorFile.close()
+
+if __name__ == "__main__":
+
+    import argparse
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", help="Choose adaptive approach from {'hessianBased', 'DWP', 'DWR'} (default 'fixedMesh')")
+    parser.add_argument("-low", help="Lower bound for index range")
+    parser.add_argument("-high", help="Upper bound for index range")
+    parser.add_argument("-nAdapt")
+    parser.add_argument("-o", help="Output data")
+    args = parser.parse_args()
+
+    op = Options(mode="advection-diffusion",
+                 approach='fixedMesh' if args.a is None else args.a,
+                 plotpvd=True if bool(args.o) else False,
+                 nAdapt= int(args.nAdapt) if args.nAdapt is not None else 1)
+
+    resolutions = range(0 if args.low is None else int(args.low), 6 if args.high is None else int(args.high))
+    solvers = {'fixedMesh': fixedMeshAD, 'hessianBased': hessianBasedAD}
+
+    errorFile = open('outdata/advection-diffusion/' + op.approach + '.txt', 'w+')
+    for i in resolutions:
+        q = solvers[op.approach](pow(2, i), op=op)
+        print("Run %d: Mean element count: %6d Objective: %.4e Timing %.1fs"
+              % (i, q['meanElements'], q['J_h'], q['solverTimer']))
+        errorFile.write('%d, %.4e\n' % (q['meanElements'], q['J_h']))
+    errorFile.close()
--- solvers/apriori.py
+++ solvers/apriori.py
@@ -1,9 +1,16 @@
 from firedrake import *
 
-from utils.adaptivity import metricGradation, steadyMetric
-from utils.interpolation import interp
-from utils.misc import indicator
+from time import clock
+
+from utils.adaptivity import *
+from utils.callbacks import *
+from utils.interpolation import interp, mixedPairInterp
+from utils.misc import indicator, indexString, peakAndDistance
 from utils.options import Options
+from utils.setup import problemDomain, RossbyWaveSolution
+
+
+__all__ = ["fixedMeshAD", "hessianBasedAD", "fixedMesh", "hessianBased"]
 
 
 def weakResidualAD(c, c_, w, op=Options(mode='advection-diffusion')):
@@ -24,7 +31,7 @@ def weakResidualAD(c, c_, w, op=Options(mode='advection-diffusion')):
     return F
 
 
-def fixedMesh(n=3, op=Options(mode='advection-diffusion', approach="fixedMesh")):
+def fixedMeshAD(n=3, op=Options(mode='advection-diffusion', approach="fixedMesh")):
     forwardFile = File(op.di + "fixedMesh.pvd")
 
     mesh = RectangleMesh(4 * n, n, 4, 1)  # Computational mesh
@@ -77,7 +84,8 @@ def fixedMesh(n=3, op=Options(mode='advection-diffusion', approach="fixedMesh"))
 
     return quantities
 
-def hessianBased(n=3, op=Options(mode='advection-diffusion', approach="hessianBased")):
+
+def hessianBasedAD(n=3, op=Options(mode='advection-diffusion', approach="hessianBased")):
     forwardFile = File(op.di + "hessianBased.pvd")
 
     # Define problem
@@ -160,5 +168,236 @@ def hessianBased(n=3, op=Options(mode='advection-diffusion', approach="hessianBa
     quantities['meanElements'] = av
     quantities['solverTimer'] = adaptSolveTimer
     quantities['J_h'] = J_h
+
+    return quantities
+
+
+from thetis import *
+
+
+def fixedMesh(startRes, **kwargs):
+    op = kwargs.get('op')
+
+    # Initialise domain and physical parameters
+    try:
+        assert float(physical_constants['g_grav'].dat.data) == op.g
+    except:
+        physical_constants['g_grav'].assign(op.g)
+    mesh, u0, eta0, b, BCs, f = problemDomain(startRes, op=op)
+    V = op.mixedSpace(mesh)
+    if op.mode == 'rossby-wave':            # Analytic final-time state
+        peak_a, distance_a = peakAndDistance(RossbyWaveSolution(V, op=op).__call__(t=op.Tend).split()[1])
+
+    # Initialise solver
+    solver_obj = solver2d.FlowSolver2d(mesh, b)
+    options = solver_obj.options
+    options.element_family = op.family
+    options.use_nonlinear_equations = True
+    options.use_grad_div_viscosity_term = True              # Symmetric viscous stress
+    options.use_lax_friedrichs_velocity = False             # TODO: This is a temporary fix
+    options.coriolis_frequency = f
+    options.simulation_export_time = op.dt * op.ndump
+    options.simulation_end_time = op.Tend - 0.5 * op.dt
+    options.timestepper_type = op.timestepper
+    options.timestep = op.dt
+    options.output_directory = op.di
+    if not op.plotpvd:
+        options.no_exports = True
+    solver_obj.assign_initial_conditions(elev=eta0, uv=u0)
+    cb1 = SWCallback(solver_obj)
+    cb1.op = op
+    if op.mode != 'tohoku':
+        cb2 = MirroredSWCallback(solver_obj)
+        cb2.op = op
+        solver_obj.add_callback(cb2, 'timestep')
+    else:
+        cb3 = P02Callback(solver_obj)
+        cb4 = P06Callback(solver_obj)
+        solver_obj.add_callback(cb3, 'timestep')
+        solver_obj.add_callback(cb4, 'timestep')
+    solver_obj.add_callback(cb1, 'timestep')
+    solver_obj.bnd_functions['shallow_water'] = BCs
+
+    # Solve and extract timeseries / functionals
+    quantities = {}
+    solverTimer = clock()
+    solver_obj.iterate()
+    solverTimer = clock() - solverTimer
+    quantities['J_h'] = cb1.quadrature()          # Evaluate objective functional
+    quantities['Integrand'] = cb1.getVals()
+    if op.mode != 'tohoku':
+        quantities['J_h mirrored'] = cb2.quadrature()
+        quantities['Integrand-mirrored'] = cb2.getVals()
+    else:
+        quantities['TV P02'] = cb3.totalVariation()
+        quantities['TV P06'] = cb4.totalVariation()
+        quantities['P02'] = cb3.getVals()
+        quantities['P06'] = cb4.getVals()
+
+    # Measure error using metrics, as in Huang et al.
+    if op.mode == 'rossby-wave':
+        peak, distance = peakAndDistance(solver_obj.fields.solution_2d.split()[1], op=op)
+        quantities['peak'] = peak/peak_a
+        quantities['dist'] = distance/distance_a
+        quantities['spd'] = distance /(op.Tend * 0.4)
+
+    # Output mesh statistics and solver times
+    quantities['meanElements'] = mesh.num_cells()
+    quantities['solverTimer'] = solverTimer
+    quantities['adaptSolveTimer'] = 0.
+
+    return quantities
+
+
+def hessianBased(startRes, **kwargs):
+    op = kwargs.get('op')
+
+    # Initialise domain and physical parameters
+    try:
+        assert float(physical_constants['g_grav'].dat.data) == op.g
+    except:
+        physical_constants['g_grav'].assign(op.g)
+    mesh, u0, eta0, b, BCs, f = problemDomain(startRes, op=op)
+    V = op.mixedSpace(mesh)
+    uv_2d, elev_2d = Function(V).split()  # Needed to load data into
+    elev_2d.interpolate(eta0)
+    uv_2d.interpolate(u0)
+    if op.mode == 'rossby-wave':    # Analytic final-time state
+        peak_a, distance_a = peakAndDistance(RossbyWaveSolution(V, op=op).__call__(t=op.Tend).split()[1])
+
+    # Initialise parameters and counters
+    nEle = mesh.num_cells()
+    op.nVerT = mesh.num_vertices() * op.rescaling   # Target #Vertices
+    mM = [nEle, nEle]  # Min/max #Elements
+    Sn = nEle
+    cnt = 0
+    t = 0.
+
+    adaptSolveTimer = 0.
+    quantities = {}
+    while cnt < op.cntT:
+        adaptTimer = clock()
+        P1 = FunctionSpace(mesh, "CG", 1)
+
+        if op.adaptField != 's':
+            height = Function(P1).assign(elev_2d)
+        if op.adaptField != 'f':
+            spd = Function(P1).interpolate(sqrt(dot(uv_2d, uv_2d)))
+        for l in range(op.nAdapt):                  # TODO: Test this functionality
+
+            # Construct metric
+            if op.adaptField != 's':
+                M = steadyMetric(height, op=op)
+            if op.adaptField != 'f' and cnt != 0:   # Can't adapt to zero velocity
+                M2 = steadyMetric(spd, op=op)
+                M = metricIntersection(M, M2) if op.adaptField == 'b' else M2
+            if op.bAdapt and not (op.adaptField != 'f' and cnt == 0):
+                M2 = steadyMetric(b, op=op)
+                M = M2 if op.adaptField != 'f' and cnt == 0. else metricIntersection(M, M2)     # TODO: Convex combination?
+
+            # Adapt mesh and interpolate variables
+            if op.bAdapt or cnt != 0 or op.adaptField == 'f':
+                mesh = AnisotropicAdaptation(mesh, M).adapted_mesh
+            if l < op.nAdapt-1:
+                if op.adaptField != 's':
+                    height = interp(mesh, height)
+                if op.adaptField != 'f':
+                    spd = interp(mesh, spd)
+
+        if cnt != 0 or op.adaptField == 'f':
+            elev_2d, uv_2d = interp(mesh, elev_2d, uv_2d)
+            b, BCs, f = problemDomain(mesh=mesh, op=op)[3:]
+            uv_2d.rename('uv_2d')
+            elev_2d.rename('elev_2d')
+        adaptTimer = clock() - adaptTimer
+
+        # Solver object and equations
+        adapSolver = solver2d.FlowSolver2d(mesh, b)
+        adapOpt = adapSolver.options
+        adapOpt.element_family = op.family
+        adapOpt.use_nonlinear_equations = True
+        adapOpt.use_grad_div_viscosity_term = True                  # Symmetric viscous stress
+        adapOpt.use_lax_friedrichs_velocity = False                 # TODO: This is a temporary fix
+        adapOpt.simulation_export_time = op.dt * op.ndump
+        adapOpt.simulation_end_time = t + op.dt * (op.rm - 0.5)
+        adapOpt.timestepper_type = op.timestepper
+        adapOpt.timestep = op.dt
+        adapOpt.output_directory = op.di
+        if not op.plotpvd:
+            adapOpt.no_exports = True
+        adapOpt.coriolis_frequency = f
+        adapSolver.assign_initial_conditions(elev=elev_2d, uv=uv_2d)
+        adapSolver.i_export = int(cnt / op.ndump)
+        adapSolver.next_export_t = adapSolver.i_export * adapSolver.options.simulation_export_time
+        adapSolver.iteration = cnt
+        adapSolver.simulation_time = t
+        for e in adapSolver.exporters.values():
+            e.set_next_export_ix(adapSolver.i_export)
+
+        # Establish callbacks and iterate
+        cb1 = SWCallback(adapSolver)
+        cb1.op = op
+        if op.mode != 'tohoku':
+            cb2 = MirroredSWCallback(adapSolver)
+            cb2.op = op
+        else:
+            cb3 = P02Callback(adapSolver)
+            cb4 = P06Callback(adapSolver)
+            if cnt == 0:
+                initP02 = cb3.init_value
+                initP06 = cb4.init_value
+        if cnt != 0:
+            cb1.objective_value = quantities['Integrand']
+            if op.mode != 'tohoku':
+                cb2.objective_value = quantities['Integrand-mirrored']
+            else:
+                cb3.gauge_values = quantities['P02']
+                cb3.init_value = initP02
+                cb4.gauge_values = quantities['P06']
+                cb4.init_value = initP06
+        adapSolver.add_callback(cb1, 'timestep')
+        if op.mode != 'tohoku':
+            adapSolver.add_callback(cb2, 'timestep')
+        else:
+            adapSolver.add_callback(cb3, 'timestep')
+            adapSolver.add_callback(cb4, 'timestep')
+        adapSolver.bnd_functions['shallow_water'] = BCs
+        solverTimer = clock()
+        adapSolver.iterate()
+        solverTimer = clock() - solverTimer
+        quantities['J_h'] = cb1.quadrature()  # Evaluate objective functional
+        quantities['Integrand'] = cb1.getVals()
+        if op.mode != 'tohoku':
+            quantities['J_h mirrored'] = cb2.quadrature()
+            quantities['Integrand-mirrored'] = cb2.getVals()
+        else:
+            quantities['P02'] = cb3.getVals()
+            quantities['P06'] = cb4.getVals()
+            quantities['TV P02'] = cb3.totalVariation()
+            quantities['TV P06'] = cb4.totalVariation()
+
+        # Get mesh stats
+        nEle = mesh.num_cells()
+        mM = [min(nEle, mM[0]), max(nEle, mM[1])]
+        Sn += nEle
+        cnt += op.rm
+        t += op.dt * op.rm
+        av = op.printToScreen(int(cnt/op.rm+1), adaptTimer, solverTimer, nEle, Sn, mM, cnt * op.dt)
+        adaptSolveTimer += adaptTimer + solverTimer
+
+        # Extract fields for next step
+        uv_2d, elev_2d = adapSolver.fields.solution_2d.split()
+
+    # Measure error using metrics, as in Huang et al.
+    if op.mode == 'rossby-wave':
+        peak, distance = peakAndDistance(elev_2d, op=op)
+        quantities['peak'] = peak / peak_a
+        quantities['dist'] = distance / distance_a
+        quantities['spd'] = distance / (op.Tend * 0.4)
+
+    # Output mesh statistics and solver times
+    quantities['meanElements'] = av
+    quantities['solverTimer'] = adaptSolveTimer
+    quantities['adaptSolveTimer'] = adaptSolveTimer
 
     return quantities
