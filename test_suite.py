6e73ce7d0c1ff7568647efed1c9a6c68f8caa1a6
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-06-20 10:27:58 +0100

    Attempt at Tohoku gradation

--- test_suite.py
+++ test_suite.py
@@ -4,7 +4,7 @@ from utils.adaptivity import isotropicMetric, metricIntersection, metricGradatio
 from utils.options import GaussianOptions, TohokuOptions
 
 
-mode = 1
+mode = 2
 
 if mode in (0, 1):
     mesh = UnitSquareMesh(30, 30)
@@ -12,18 +12,25 @@ if mode in (0, 1):
 elif mode == 2:
     mesh = Mesh('resources/meshes/Tohoku0.msh')
     op = TohokuOptions()
-op.maxGrowth = 1.4
+op.maxGrowth = 1.001
 
 P1 = FunctionSpace(mesh, "CG", 1)
 op.nVerT = mesh.num_vertices()
 
+h = Function(P1).interpolate(CellSize(mesh))
+# print("min = ", min(h.dat.data))
+# print("max = ", max(h.dat.data))
+# exit(0)
+
 f = Function(P1)
 if mode == 0:
     f.interpolate(Expression("pow(x[0], 2) + pow(x[1], 2) < 0.4 ? 1000. : 5."))
-if mode == 1:
+elif mode == 1:
     f.assign(100.)
+elif mode == 2:
+    f.assign(100000.)
 
-M_f = isotropicMetric(f, invert=False, op=op)
+M_f = isotropicMetric(f, invert=False if mode in (0,1) else True, op=op)
 mesh_f = AnisotropicAdaptation(mesh, M_f).adapted_mesh
 File('plots/mesh_f.pvd').write(mesh_f.coordinates)
 File('plots/metric_f.pvd').write(M_f)
@@ -33,8 +40,10 @@ if mode == 0:
     g.interpolate(Expression("pow(x[0] - 1., 2) + pow(x[1] - 1., 2) < 0.4 ? 1000. : 5."))
 elif mode == 1:
     g.assign(10000.)
+elif mode == 2:
+    g.assign(h)
 
-M_g = isotropicMetric(g, invert=False, op=op)
+M_g = isotropicMetric(g, invert=False if mode in (0,1) else True, op=op)
 mesh_g = AnisotropicAdaptation(mesh, M_g).adapted_mesh
 File('plots/mesh_g.pvd').write(mesh_g.coordinates)
 File('plots/metric_g.pvd').write(M_g)
@@ -43,6 +52,8 @@ if mode == 0:
     M_fg = metricIntersection(M_f, M_g)
 elif mode == 1:
     M_fg = metricIntersection(M_f, M_g, bdy=1)
+elif mode == 2:
+    M_fg = metricIntersection(M_f, M_g, bdy=200)
 mesh_fg = AnisotropicAdaptation(mesh, M_fg).adapted_mesh
 File('plots/mesh_fg.pvd').write(mesh_fg.coordinates)
 File('plots/metric_fg.pvd').write(M_fg)
