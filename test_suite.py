38b916f6914d69c0e197d334a67f82386c60f414
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2018-06-20 10:10:19 +0100

    Boundary test case

--- test_suite.py
+++ test_suite.py
@@ -1,19 +1,27 @@
 from firedrake import *
 
 from utils.adaptivity import isotropicMetric, metricIntersection, metricGradation, normaliseIndicator, metricComplexity
-from utils.options import GaussianOptions
+from utils.options import GaussianOptions, TohokuOptions
 
 
-mesh = UnitSquareMesh(30, 30)
+mode = 1
+
+if mode in (0, 1):
+    mesh = UnitSquareMesh(30, 30)
+    op = GaussianOptions()
+elif mode == 2:
+    mesh = Mesh('resources/meshes/Tohoku0.msh')
+    op = TohokuOptions()
+op.maxGrowth = 1.4
+
 P1 = FunctionSpace(mesh, "CG", 1)
-op = GaussianOptions()
-op.maxGrowth = 1.
 op.nVerT = mesh.num_vertices()
 
 f = Function(P1)
-f.interpolate(Expression("pow(x[0], 2) + pow(x[1], 2) < 0.4 ? 1000. : 5."))
-# f.assign(100.)
-# f = normaliseIndicator(f, op=op)
+if mode == 0:
+    f.interpolate(Expression("pow(x[0], 2) + pow(x[1], 2) < 0.4 ? 1000. : 5."))
+if mode == 1:
+    f.assign(100.)
 
 M_f = isotropicMetric(f, invert=False, op=op)
 mesh_f = AnisotropicAdaptation(mesh, M_f).adapted_mesh
@@ -21,18 +29,20 @@ File('plots/mesh_f.pvd').write(mesh_f.coordinates)
 File('plots/metric_f.pvd').write(M_f)
 
 g = Function(P1)
-g.interpolate(Expression("pow(x[0] - 1., 2) + pow(x[1] - 1., 2) < 0.4 ? 1000. : 5."))
-# g.assign(10000.)
-# g = normaliseIndicator(g, op=op)
+if mode == 0:
+    g.interpolate(Expression("pow(x[0] - 1., 2) + pow(x[1] - 1., 2) < 0.4 ? 1000. : 5."))
+elif mode == 1:
+    g.assign(10000.)
 
 M_g = isotropicMetric(g, invert=False, op=op)
 mesh_g = AnisotropicAdaptation(mesh, M_g).adapted_mesh
 File('plots/mesh_g.pvd').write(mesh_g.coordinates)
 File('plots/metric_g.pvd').write(M_g)
 
-M_fg = metricIntersection(M_f, M_g)
-# M_fg = metricIntersection(M_f, M_g, bdy=1)
-# M_fg.dat.data[:] *= mesh.num_vertices() / metricComplexity(M_fg)
+if mode == 0:
+    M_fg = metricIntersection(M_f, M_g)
+elif mode == 1:
+    M_fg = metricIntersection(M_f, M_g, bdy=1)
 mesh_fg = AnisotropicAdaptation(mesh, M_fg).adapted_mesh
 File('plots/mesh_fg.pvd').write(mesh_fg.coordinates)
 File('plots/metric_fg.pvd').write(M_fg)
